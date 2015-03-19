b9b9935e1ed1863d0c89fdbc01362a6638cc7d03
Park <ppark@natl2236lppark.cmg.int>
2015-03-19 15:07:12 -0400

    added export to excel feature

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/underscore/underscore.js',
       'bower_components/angular-busy/dist/angular-busy.js',
       'bower_components/angular-native-picker/build/angular-datepicker.js',
+      'bower_components/excellentexport/excellentexport.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
